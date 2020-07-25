b15a798c1ea87a3700ad95a354ec3ec4fe67dabd
vrsvr <rifatvarisov@gmail.com>
2020-07-25 16:06:52 +0500

    Модальное окно "Меню"

--- js/main.js
+++ js/main.js
@@ -182,7 +182,7 @@ $(function() {
 
 //Модальные окна на Pure Js
 
-// (function() {
+(function() {
   
 //   //Вызов окна колбека
 //   openCallback = document.querySelector('.openCallback');
@@ -210,78 +210,78 @@ $(function() {
 //   };
 
 //   //Вызов окна колбека
-//   openLeftMenu = document.querySelector('.openMenu');
-//   leftMenuModal = document.querySelector('.modal-wrapper__left-menu');
+  openLeftMenu = document.querySelector('.openMenu');
+  leftMenuModal = document.querySelector('.modal-wrapper__left-menu');
 
-//   openLeftMenu.addEventListener('click', function () {
-//     openBaseModal();
-//     leftMenuModal.classList.remove('hidden');
-//     setTimeout(function () {
-//       leftMenuModal.classList.remove('animation');
-//     }, 20);
-//   })
+  openLeftMenu.addEventListener('click', function () {
+    openBaseModal();
+    leftMenuModal.classList.remove('hidden');
+    setTimeout(function () {
+      leftMenuModal.classList.remove('animation');
+    }, 20);
+  })
 
-//   function closeleftMenuModal() {
-//     if (!leftMenuModal.classList.contains('hidden')) {
-//       leftMenuModal.classList.add('animation');    
-//       leftMenuModal.addEventListener('transitionend', function(e) {
-//         leftMenuModal.classList.add('hidden');
-//       }, {
-//         capture: false,
-//         once: true,
-//         passive: false
-//       });
-//     }
+  function closeleftMenuModal() {
+    if (!leftMenuModal.classList.contains('hidden')) {
+      leftMenuModal.classList.add('animation');    
+      leftMenuModal.addEventListener('transitionend', function(e) {
+        leftMenuModal.classList.add('hidden');
+      }, {
+        capture: false,
+        once: true,
+        passive: false
+      });
+    }
 
-//   };
+  };
 
-//   function closeAllModal() {
-//     closecallbackPopup();
-//     closeleftMenuModal();
-//     closeBaseModal();
-//   };
+  function closeAllModal() {
+    // closecallbackPopup();
+    closeleftMenuModal();
+    closeBaseModal();
+  };
 
-//   //База модальных окон
-//   body = document.querySelector('body');
-//   modalWrapper = document.querySelector('.modal-wrapper');
-//   modalWrapperBg = document.querySelector('.modal-wrapper__bg');
-//   modalWrapperClose = document.querySelectorAll('.modal-wrapper__close');
+  //База модальных окон
+  body = document.querySelector('body');
+  modalWrapper = document.querySelector('.modal-wrapper');
+  modalWrapperBg = document.querySelector('.modal-wrapper__bg');
+  modalWrapperClose = document.querySelectorAll('.modal-wrapper__close');
 
-//   function openBaseModal() {
-//     body.classList.add('noflow');
-//     modalWrapper.classList.remove('hidden');
-//     setTimeout(function () {
-//       modalWrapper.classList.remove('animation');
-//     }, 20);
-//   };
+  function openBaseModal() {
+    body.classList.add('noflow');
+    modalWrapper.classList.remove('hidden');
+    setTimeout(function () {
+      modalWrapper.classList.remove('animation');
+    }, 20);
+  };
 
-//   function closeBaseModal() {
-//     body.classList.remove('noflow');
-//     modalWrapper.classList.add('animation');    
-//     modalWrapper.addEventListener('transitionend', function(e) {
-//       modalWrapper.classList.add('hidden');
-//     }, {
-//       capture: false,
-//       once: true,
-//       passive: false
-//     });
-//   };
+  function closeBaseModal() {
+    body.classList.remove('noflow');
+    modalWrapper.classList.add('animation');    
+    modalWrapper.addEventListener('transitionend', function(e) {
+      modalWrapper.classList.add('hidden');
+    }, {
+      capture: false,
+      once: true,
+      passive: false
+    });
+  };
 
-//   for(let i = 0; i<modalWrapperClose.length; i++){
-//     modalWrapperClose[i].addEventListener('click', () => {
-//       closeAllModal();
-//     });
-//   }
+  for(let i = 0; i<modalWrapperClose.length; i++){
+    modalWrapperClose[i].addEventListener('click', () => {
+      closeAllModal();
+    });
+  }
 
-//   modalWrapperBg.addEventListener('click', function () {
-//     closeAllModal();
-//   })
+  modalWrapperBg.addEventListener('click', function () {
+    closeAllModal();
+  })
 
-//   document.onkeydown = function(e) {
-//     e = e || window.event;
-//     if (e.key=='Escape'||e.key=='Esc'||e.keyCode==27) {
-//       closeAllModal();
-//     }
-//   };
+  document.onkeydown = function(e) {
+    e = e || window.event;
+    if (e.key=='Escape'||e.key=='Esc'||e.keyCode==27) {
+      closeAllModal();
+    }
+  };
 
-// })();
+})();
