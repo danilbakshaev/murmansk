76829e4e4a2e3c49f806776e25002a9f5d2b385b
vladislavkostromin <61909342+vladislavkostromin@users.noreply.github.com>
2020-09-30 23:53:12 +0500

    исправления блоки 2й 3й 4й

--- js/main.js
+++ js/main.js
@@ -3,25 +3,23 @@ $(function () {
   if ($('.popular-destinations__items')) {
     $('.popular-destinations__items').slick({
       slidesToShow: 3.4,
-      slidesToScroll: 1,
+      slidesToScroll: 3.4,
       infinite: false,
+      draggable: false,
       prevArrow: '.prev-js-popular-destinations',
       nextArrow: '.next-js-popular-destinations',
       responsive: [{
           breakpoint: 1240,
           settings: {
-            slidesToShow: 2.2,
-            slidesToScroll: 1,
-            centerMode: false,
+            slidesToShow: 2.1,
+            slidesToScroll: 2.1,
           }
         },
         {
           breakpoint: 740,
           settings: {
-            slidesToShow: 1.2,
-            slidesToScroll: 1,
-            arrows: false,
-            centerMode: false,
+            slidesToShow: 1.1,
+            slidesToScroll: 1.1,
           }
         },
       ]
@@ -59,25 +57,23 @@ $(function () {
   if ($('.popular-destinations__items--readytours-slider')) {
     $('.popular-destinations__items--readytours-slider').slick({
       slidesToShow: 3.4,
-      slidesToScroll: 1,
+      slidesToScroll: 3.4,
       infinite: false,
+      draggable: false,
       prevArrow: '.prev-js-popular-readytours-header',
       nextArrow: '.next-js-popular-readytours-header',
       responsive: [{
           breakpoint: 1240,
           settings: {
-            slidesToShow: 2.2,
-            slidesToScroll: 1,
-            centerMode: false,
+            slidesToShow: 2.1,
+            slidesToScroll: 2.1,
           }
         },
         {
           breakpoint: 740,
           settings: {
-            slidesToShow: 1.2,
-            slidesToScroll: 1,
-            arrows: false,
-            centerMode: false,
+            slidesToShow: 1.1,
+            slidesToScroll: 1.1,
           }
         },
       ]
